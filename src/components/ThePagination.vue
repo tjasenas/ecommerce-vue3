cfd00018e8118c1a61ed6a0514cd7d4e474ec90c
tjasenas <tomasjasenas@gmail.com>
2023-05-03 20:52:21 +0300

    new

--- src/components/ThePagination.vue
+++ src/components/ThePagination.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="pagination">
-    <li v-for="page in pages" :key="page" @click="$emits('updatePage')">
+    <li v-for="page in pages" :key="page" @click="$emit('updatePage')">
       <router-link v-if="page !== activePage" :to="`/page/${page}`">{{ page }}</router-link>
       <span v-else class="active">{{ page }}</span>
     </li>
